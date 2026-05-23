08c35fde63527234eb74ca5ca399b3f502a1dbbd
hoangvu1118 <mhoang.vuduc@gmail.com>
2026-05-23 22:06:08 +0700

    adding register_node method to send ip:port to java backend server

--- dfl_node.py
+++ dfl_node.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     
     # Start the reporter BEFORE running the training loops
     dashboard_url = "http://127.0.0.1:3000/api/report"
-    reporter.start(node_id=NODE_ID, dashboard_url=dashboard_url, interval=5)  # Set to 5s for faster testing
+    reporter.start(node_id=NODE_ID, dashboard_url=dashboard_url, port= PORT, interval=5)  # Set to 5s for faster testing
 
     run_rounds(cfg)
 
--- lib/reporter.py
+++ lib/reporter.py
@@ -2,6 +2,7 @@ import threading
 import time
 import requests
 import logging
+import socket
 from lib.state import get_state
 
 log = logging.getLogger(__name__)
@@ -14,20 +15,43 @@ class Reporter:
     def __init__(self):
         self.node_id = None
         self.dashboard_url = None
+        self.ip_address = None
+        self.port = None
         self.interval = 10
         self._thread = None
         self._stop_event = threading.Event()
 
-    def start(self, node_id: int, dashboard_url: str, interval: int = 10):
+    def start(self, node_id: int, dashboard_url: str, port: int, interval: int = 10):
         """Initializes settings and starts the periodic heartbeat thread"""
         self.node_id = node_id
         self.dashboard_url = dashboard_url
         self.interval = interval
+        
+        self.ip_address = self.get_current_ip(dashboard_url, port)
+        self.port = port
+
+        if self.ip_address and self.port:
+            self.register_node()
 
         if self.dashboard_url:
             self._thread = threading.Thread(target=self._heartbeat_loop, daemon=True)
             self._thread.start()
             log.info(f"Reporter started, reporting to {dashboard_url} every {interval}s")
+
+    def get_current_ip(target_host, target_port):
+        # Create a fake UDP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # UDP is connectionless protocol -> "If we were to send a packet to server -> which IP"
+            s.connect((target_host, target_port))
+            # Then retrieve the IP from the OS
+            ip = s.getsockname()[0]
+        except Exception:
+            # Otherwise, hardcode -> replace with the private network later
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+        return ip
 
     def stop(self):
         """Stops the periodic heartbeat loop."""
@@ -44,6 +68,20 @@ class Reporter:
             if not self._stop_event.is_set():
                 state = get_state()
                 self.report_status(state["phase"], state["round"])
+    
+    def register_node(self):
+        if not self.dashboard_url:
+            return
+        
+        payload = {
+            "node_id": self.node_id,
+            "ip_address" : self.ip_address,
+            "port" : self.port
+        }
+        try:
+            requests.post(self.dashboard_url, json=payload, timeout = 5)
+        except Exception as e:
+            log.debug(f"Failed to register node to dashboard: {e}")
 
     def report_status(self, phase: str, round_num: int):
         """Sends data immediately to the dashboard if configured."""
